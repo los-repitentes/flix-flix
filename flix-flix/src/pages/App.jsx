0bffbf1ce9dd80836db9113f1f6ae02ce2118397
JelsinJpm <jparramosquera@gmail.com>
2023-12-01 14:37:23 -0500

    Prueba

--- flix-flix/src/pages/App.jsx
+++ flix-flix/src/pages/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more !HELLO¡
       </p>
     </>
   )
