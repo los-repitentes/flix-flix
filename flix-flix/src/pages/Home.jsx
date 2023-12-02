4fb05f33f85b39f0f1824e17d2c25619dc628f27
JelsinJpm <jparramosquera@gmail.com>
2023-12-01 21:50:34 -0500

    Configuracion Banner

--- flix-flix/src/pages/Home.jsx
+++ flix-flix/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header"
 import Footer from "../components/footer/Footer"
 import MovieSection from "../components/movieSection/MovieSection";
+
 const Home = () => {
   return (
     <>
