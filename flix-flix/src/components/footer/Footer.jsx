e34b57842f02a8199a9fcf7b23ab6669e3f720c2
phelipe009 <phelipecordoba009@gmail.com>
2023-12-02 09:09:44 -0500

    actualizacion footer

--- flix-flix/src/components/footer/Footer.jsx
+++ flix-flix/src/components/footer/Footer.jsx
@@ -1,26 +1,57 @@
-import React from 'react'
+
+import React from 'react';
+import './footer.css';
 
 const Footer = () => {
   return (
-    <>
-        <footer className="footer">
-            <div className="container">
-                <div className="left">
-                <p>&copy; 2023 Los Repitentes. Todos los derechos reservados.</p>
-                </div>
-                <div className="right">
-                <p>Síguenos en:</p>
-                <ul className="social-list">
-                    <li className="social-item"><a href="https://twitter.com/test" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                    <li className="social-item"><a href="https://facebook.com/test" target="_blank" rel="noopener noreferrer">Facebook</a></li>
-                    <li className="social-item"><a href="https://linkedin.com/company/test" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-                </ul>
-                </div>
-            </div>
-            <p className="additional">¡Gracias por visitarnos!</p>
-            </footer>
-    </>
-  )
-}
+    <footer className="section footer-minimal context-dark">
+      <div className="container wow-outer">
+        <div className="wow fadeIn">
+          <div className="row row-50 row-lg-60">
 
-export default Footer
+            {/* Logo Section */}
+            <div className="col-12">
+              <a href="home">
+                <img src="src\assets\logo.svg" alt="" width="207" height="51"/>
+              </a>
+            </div>
+
+            {/* Navigation Links Section */}
+            <div className="col-12">
+              <ul className="footer-minimal-nav">
+                <li><a href="home">Inicio</a></li>
+                <li><a href="#">Mas vistos</a></li>
+                <li><a href="#">Favoritos</a></li>
+              </ul>
+            </div>
+
+            {/* Social Media Links Section */}
+            <div className="col-12">
+              <ul className="social-list">
+                <li><a className="icon icon-sm icon-circle icon-circle-md icon-bg-white fa-facebook" href=""></a></li>
+                <li><a className="icon icon-sm icon-circle icon-circle-md icon-bg-white fa-instagram" href="#"></a></li>
+                <li><a className="icon icon-sm icon-circle icon-circle-md icon-bg-white fa-twitter" href="#"></a></li>
+              </ul>
+            </div>
+
+          </div>
+
+          {/* Copyright Information Section */}
+          <p className="rights">
+            <span>&copy;&nbsp;</span>
+            <span className="copyright-year"></span>
+            <span>&nbsp;</span>
+            <span>Todos</span>
+            <span>.&nbsp;</span>
+            <span>Los derechos reservados.</span>
+            <span>&nbsp;</span>
+            Desarrollado&nbsp;por&nbsp;<a href="home">Los Repitentes</a>
+          </p>
+
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
