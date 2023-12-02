74f58f589d6c9291eebb78843a283fac19c0c39c
lcchaverra <lucas.cordoba1510@gmail.com>
2023-12-01 20:40:15 -0500

    actualizacion del main

--- flix-flix/src/pages/Home.jsx
+++ flix-flix/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header"
-
+import Footer from "../components/footer/Footer"
+import MovieSection from "../components/movieSection/MovieSection";
 const Home = () => {
   return (
     <>
       <Header/>
-      <p>Home</p>
+      <MovieSection/>
+      <Footer/>
     </>
   )
 }
